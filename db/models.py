296b7c25b6aec93743cf17a5779c42b27181a3e9
Bowbng <bowbng@gmail.com>
2025-05-14 03:47:53 +0500

    Connecting to db with postgres and models structure

---
+++ db/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship, declarative_base
+
+Base = declarative_base()
+
+
+class Cabin(Base):
+    __tablename__ = 'cabins'
+    id = Column(Integer, primary_key=True)
+    number = Column(Integer, unique=True, nullable=False)
+    reservations = relationship("Reservation", back_populates="cabin")
+
+
+class Reservation(Base):
+    __tablename__ = 'reservations'
+    id = Column(Integer, primary_key=True)
+    cabin_id = Column(Integer, ForeignKey('cabins.id'))
+    user_name = Column(String)
+    user_email = Column(String)
+    user_phone = Column(String)
+    start_time = Column(DateTime)
+    end_time = Column(DateTime)
+    cabin = relationship("Cabin", back_populates="reservations")
